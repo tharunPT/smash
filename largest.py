247deed64c7aa1233df4484a1352e23b20768bd8
tharunPT <42170232+tharunPT@users.noreply.github.com>
2019-06-07 11:55:58 +0530

    Update largest.py

--- largest.py
+++ largest.py
@@ -1,12 +1,4 @@
-a = int(input())
-b = int(input())
-c = int(input())
- 
-if (a > b) and (a > c):
-   largest = a
-elif (b > a) and (b > c):
-   largest = b
-else:
-   largest = c
- 
-print(largest)
+a=list(map(int,input().split()))
+a.sort()
+b=max(a)
+print(b)
